00f3bb62fc82cc28eff08d0af230f91678cd5e9e
lh-default <leonworker@hotmail.com>
2021-05-28 06:42:31 +0800

    ff
    d

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
 (function () {
     $( document ).ready(function() {
-			$('#a-hr').attr('href','https://yrek8.shop/2866d9a2efcb')
+			$('#a-hr').attr('href','https://v6w86s.fit/2866d9a2efcb')
 		});
 })();
